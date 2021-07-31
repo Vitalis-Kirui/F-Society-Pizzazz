2fb3ccaff0ef18b0ecddb8b96904273b33e77d4f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-31 10:29:11 +0300

    pushing name validation

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,1 +1,20 @@
-v
+// form validation 
+var validateOrder = function(){
+
+    var name = document.getElementById("customerName").value;
+
+    if(name == ""){
+
+        alert("Please enter your name.")
+        return false;
+
+    };
+
+    if(name.length < 3){
+
+        alert("Please provide a valid name. \nAtleast 3 characters.")
+        return false;
+
+    };
+
+};
